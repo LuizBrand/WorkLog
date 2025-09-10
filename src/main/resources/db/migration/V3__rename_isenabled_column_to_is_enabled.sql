8647b32dbefb6df47886cb5c2e3df1b17cf43f4e
Luiz Lira <lbrandaolira@gmail.com>
2025-09-09 20:50:34 -0400

    chore(db): Add migrations for is_enabled column

---
+++ src/main/resources/db/migration/V3__rename_isenabled_column_to_is_enabled.sql
@@ -0,0 +1,3 @@
+ALTER TABLE users RENAME COLUMN "isenabled" TO is_enabled;
+ALTER TABLE clients RENAME COLUMN "isenabled" TO is_enabled;
+ALTER TABLE tickets RENAME COLUMN "isenabled" TO is_enabled;
