a84a6ea268d6770657be85c9c22490dd60fdf0db
Luiz Lira <lbrandaolira@gmail.com>
2026-01-13 19:30:23 -0400

    feat: add TicketLog entity and repository

---
+++ src/main/java/br/com/luizbrand/worklog/tickets/TicketLog.java
@@ -0,0 +1,52 @@
+package br.com.luizbrand.worklog.tickets;
+
+import br.com.luizbrand.worklog.tickets.enums.FieldType;
+import br.com.luizbrand.worklog.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+@Table(name = "ticket_logs")
+public class TicketLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "change_group_id", nullable = false)
+    private UUID changeGroupId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ticket_id", nullable = false)
+    private Ticket ticket;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "field_changed")
+    private String fieldChanged;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "field_type", nullable = false, length = 20)
+    private FieldType fieldType;
+
+    @Column(name = "old_value", columnDefinition = "TEXT")
+    private String oldValue;
+
+    @Column(name = "new_value", columnDefinition = "TEXT")
+    private String newValeu;
+
+    @Column(name = "change_date")
+    private LocalDateTime changeDate;
+
+
+}
