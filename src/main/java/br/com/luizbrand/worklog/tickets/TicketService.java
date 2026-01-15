b27df6f94e0aaee4b6e7d2365805f279fbb01454
Luiz Lira <lbrandaolira@gmail.com>
2026-01-14 21:28:26 -0400

    feat: implement ticket update logic in TicketService

--- src/main/java/br/com/luizbrand/worklog/tickets/TicketService.java
+++ src/main/java/br/com/luizbrand/worklog/tickets/TicketService.java
@@ -7,6 +7,8 @@ import br.com.luizbrand.worklog.system.SystemService;
 import br.com.luizbrand.worklog.system.Systems;
 import br.com.luizbrand.worklog.tickets.dto.TicketRequest;
 import br.com.luizbrand.worklog.tickets.dto.TicketResponse;
+import br.com.luizbrand.worklog.tickets.dto.TicketUpdateRequest;
+import br.com.luizbrand.worklog.user.User;
 import br.com.luizbrand.worklog.user.UserService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,14 +23,16 @@ public class TicketService {
     private final ClientService clientService;
     private final SystemService systemService;
     private final UserService userService;
+    private final TicketLogManager ticketLogManager;
 
-    public TicketService(TicketRepository ticketRepository, TicketMapper ticketMapper, ClientService clientService, SystemService systemService, UserService userService) {
+    public TicketService(TicketRepository ticketRepository, TicketMapper ticketMapper, ClientService clientService, SystemService systemService, UserService userService, TicketLogManager ticketLogManager) {
 
         this.ticketRepository = ticketRepository;
         this.ticketMapper = ticketMapper;
         this.clientService = clientService;
         this.systemService = systemService;
         this.userService = userService;
+        this.ticketLogManager = ticketLogManager;
     }
 
     @Transactional
@@ -45,6 +49,8 @@ public class TicketService {
             ticket.setUser(userService.findActiveUser(ticketRequest.userId()));
         }
 
+        //TODO:verificar se o valor do status é valido
+
         return ticketMapper.toResponse(ticketRepository.save(ticket));
     }
 
@@ -52,6 +58,54 @@ public class TicketService {
         Ticket ticket = ticketRepository.findByPublicId(publicId)
                 .orElseThrow(() -> new TicketNotFoundException("Ticket not found with publicId: " + publicId));
         return ticketMapper.toResponse(ticket);
+    }
+
+    @Transactional
+    public TicketResponse updateTicket(UUID ticketPublicId, TicketUpdateRequest ticketRequest) {
+
+        Ticket existingTicket = ticketRepository.findByPublicId(ticketPublicId)
+                .orElseThrow(() -> new TicketNotFoundException("Ticket not found with publicId: " + ticketPublicId));
+
+        //TODO: Mudar para buscar no security context o user loggado
+        User currentUser = null;
+
+        if (ticketRequest.userId() != null) {
+            currentUser = userService.findEntityByPublicId(ticketRequest.userId());
+        } else {
+            currentUser = existingTicket.getUser();
+        }
+
+        Ticket newTicket = prepareNewTicket(existingTicket, ticketRequest);
+
+        //Chamar o log manager para registrar os logs
+        ticketLogManager.generateLogs(existingTicket, newTicket, currentUser);
+        //Atualiza o ticket realmente
+        updateTicketEntity(existingTicket, ticketRequest);
+
+        Ticket savedTicket = ticketRepository.save(existingTicket);
+
+        return ticketMapper.toResponse(savedTicket);
+    }
+
+    private Ticket prepareNewTicket(Ticket existingTicket, TicketUpdateRequest ticketRequest) {
+        return Ticket.builder()
+                .title(ticketRequest.title() != null ? ticketRequest.title() : existingTicket.getTitle())
+                .description(ticketRequest.description() != null ? ticketRequest.description() : existingTicket.getDescription())
+                .solution(ticketRequest.solution() != null ? ticketRequest.solution() : existingTicket.getSolution())
+                .status(ticketRequest.status() != null ? ticketRequest.status() : existingTicket.getStatus())
+                .completedAt(ticketRequest.completedAt() != null ? ticketRequest.completedAt() : existingTicket.getCompletedAt())
+                .client(existingTicket.getClient())
+                .system(existingTicket.getSystem())
+                .build();
+
+    }
+
+    private void  updateTicketEntity(Ticket ticket, TicketUpdateRequest request) {
+        if (request.title() != null) ticket.setTitle(request.title());
+        if (request.description() != null) ticket.setDescription(request.description());
+        if (request.solution() != null) ticket.setSolution(request.solution());
+        if (request.status() != null) ticket.setStatus(request.status());
+        if (request.completedAt() != null) ticket.setCompletedAt(request.completedAt());
     }
 
 }
