fc09d6ceb25c0187d45b35ea6012fc2d1e27713c
Luiz Lira <lbrandaolira@gmail.com>
2026-01-14 21:28:38 -0400

    feat: add update ticket endpoint to TicketController

--- src/main/java/br/com/luizbrand/worklog/tickets/TicketController.java
+++ src/main/java/br/com/luizbrand/worklog/tickets/TicketController.java
@@ -2,6 +2,7 @@ package br.com.luizbrand.worklog.tickets;
 
 import br.com.luizbrand.worklog.tickets.dto.TicketRequest;
 import br.com.luizbrand.worklog.tickets.dto.TicketResponse;
+import br.com.luizbrand.worklog.tickets.dto.TicketUpdateRequest;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -29,6 +30,13 @@ public class TicketController {
     public ResponseEntity<TicketResponse> getTicketByPublicId(@PathVariable UUID publicId) {
         TicketResponse ticketResponse = ticketService.findTicketByPublicId(publicId);
         return ResponseEntity.ok(ticketResponse);
+    }
+
+    @PutMapping("/update/{ticketPublicId}")
+    public ResponseEntity<TicketResponse> updateTicket(@PathVariable UUID ticketPublicId, @RequestBody @Valid TicketUpdateRequest ticketRequest) {
+        TicketResponse ticketResponse = ticketService.updateTicket(ticketPublicId, ticketRequest);
+        return ResponseEntity.ok(ticketResponse);
+
     }
 
 }
