ae9fdd26de2a47ac0c880ea378d90c43048c73ac
Luiz Lira <lbrandaolira@gmail.com>
2026-03-07 23:37:36 -0400

    last changes in windows

--- src/main/java/br/com/luizbrand/worklog/user/UserRepository.java
+++ src/main/java/br/com/luizbrand/worklog/user/UserRepository.java
@@ -1,6 +1,7 @@
 package br.com.luizbrand.worklog.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import javax.swing.text.html.Option;
@@ -10,6 +11,7 @@ import java.util.UUID;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    @Query("SELECT u FROM User u JOIN FETCH u.roles WHERE u.email = :email")
     Optional<User> findByEmail(String email);
     Optional<User> findByPublicId(UUID publicId);
 
